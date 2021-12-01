2b40a27b5ae280128a778a53bd720a726a5fa115
Daniel Jones <danielrjones1@gmail.com>
2021-12-01 09:08:10 +0100

    Puzzle 01 part 2 and tidy up

--- 01/main.py
+++ 01/main.py
@@ -1,15 +1,28 @@
-def main():
-    last_num = None
-    inc_count = 0
-    with open("input.txt") as f:
-        for line in f.readlines():
-            num = int(line)
-            if last_num is not None and num > last_num:
-                inc_count += 1
-            last_num = num
+def triplewise(it):
+    try:
+        a, b = next(it), next(it)
+    except StopIteration:
+        return
+    for c in it:
+        yield a, b, c
+        a, b = b, c
 
-    print(inc_count)
-    exit(0)
+
+def inc_count(values):
+    last = None
+    count = 0
+    for v in values:
+        if last is not None and v > last:
+            count += 1
+        last = v
+    return count
+
+
+def main():
+    nums = [int(line) for line in open("input.txt").readlines()]
+    sums = [sum(triple) for triple in triplewise(iter(nums))]
+    print(inc_count(nums))  # part 1
+    print(inc_count(sums))  # part 2
 
 
 if __name__ == "__main__":
