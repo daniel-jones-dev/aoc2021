12370acf41293edf9037426d70cc586b35b8a7db
Daniel Jones <danielrjones1@gmail.com>
2021-12-06 22:36:41 +0100

    Puzzle 05 part 02 + refactoring

--- 05/main.py
+++ 05/main.py
@@ -1,14 +1,26 @@
-def line_cells(x1, y1, x2, y2):
-    if x1 == x2:
-        if y2 > y1:
-            return [(x1, y) for y in range(y1, y2 + 1)]
+def line_cells(line):
+    x1, y1, x2, y2 = line
+    count = max(abs(x2 - x1), abs(y2 - y1)) + 1
+
+    def coords(v1, v2):
+        if v2 == v1:
+            return [v1 for _ in range(count)]
         else:
-            return [(x1, y) for y in range(y2, y1 + 1)]
-    else:
-        if x2 > x1:
-            return [(x, y1) for x in range(x1, x2 + 1)]
-        else:
-            return [(x, y1) for x in range(x2, x1 + 1)]
+            s = 1 if v2 > v1 else -1
+            return range(v1, v2 + s, s)
+
+    return zip(coords(x1, x2), coords(y1, y2))
+
+
+def print_map_value_count(map_values):
+    print(sum(sum([1 if v >= 2 else 0 for v in m]) for m in map_values))
+
+
+def add_lines_to_map_and_print(map_values, lines):
+    for line in lines:
+        for x, y in line_cells(line):
+            map_values[y][x] += 1
+    print_map_value_count(map_values)
 
 
 def main():
@@ -19,24 +31,19 @@ def main():
             x1, y1, x2, y2 = map(int, parts)
             lines.append((x1, y1, x2, y2))
 
-    # remove diagonal lines
-    lines = [
+    non_diag_lines = [
         (x1, y1, x2, y2) for x1, y1, x2, y2 in lines if x1 == x2 or y1 == y2
+    ]
+    diag_lines = [
+        (x1, y1, x2, y2) for x1, y1, x2, y2 in lines if x1 != x2 and y1 != y2
     ]
 
     max_x = max(max(x1, x2) for x1, y1, x2, y2 in lines) + 1
     max_y = max(max(y1, y2) for x1, y1, x2, y2 in lines) + 1
-
     map_values = [[0 for _ in range(max_x)] for _ in range(max_y)]
 
-    for line in lines:
-        x1, y1, x2, y2 = line
-        for x, y in line_cells(x1, y1, x2, y2):
-            map_values[y][x] += 1
-
-    print(sum(sum([1 if v >= 2 else 0 for v in m]) for m in map_values))
-
-    exit(0)
+    add_lines_to_map_and_print(map_values, non_diag_lines)
+    add_lines_to_map_and_print(map_values, diag_lines)
 
 
 if __name__ == "__main__":
