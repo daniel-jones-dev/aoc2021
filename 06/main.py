8ead38285ea1a997b79c56e522c1832f1af89ec3
Daniel Jones <danielrjones1@gmail.com>
2021-12-07 20:38:29 +0100

    Puzzle 06 part 02

--- 06/main.py
+++ 06/main.py
@@ -1,32 +1,24 @@
-fish = []
-
-
-def print_fish(day_i):
-    global fish
-    label = "Initial state: " if day_i == 0 else f"After {day_i:02} days: "
-    print(label + ",".join(map(str, fish)))
 
 
 def main():
-    global fish
     fish = [int(s) for s in open("input.txt").readline().split(",")]
 
-    last_day = 80
+    fish_counts = dict(
+        [(age, sum(1 for f in fish if f == age)) for age in range(9)]
+    )
 
+    last_day = 256
     for day_i in range(1000):
-        print_fish(day_i)
         if day_i == last_day:
-            print(len(fish))
+            print(sum(fish_counts.values()))
             break
-        num_fish = len(fish)
-        for i in range(num_fish):
-            age = fish[i]
-            if age == 0:
-                new_age = 6
-                fish.append(8)
-            else:
-                new_age = age - 1
-            fish[i] = new_age
+
+        new_fish = fish_counts[0]
+
+        for age in range(8):
+            fish_counts[age] = fish_counts[age+1]
+        fish_counts[6] += new_fish
+        fish_counts[8] = new_fish
 
 
 if __name__ == "__main__":
