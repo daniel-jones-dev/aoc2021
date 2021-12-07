b856cb5d8fd67cc8ba08bcd8346387c309c61ab3
Daniel Jones <danielrjones1@gmail.com>
2021-12-07 08:56:52 +0100

    Puzzle 07 part 02 + refactoring

--- 07/main.py
+++ 07/main.py
@@ -1,24 +1,49 @@
-def cost_at(align, positions):
+def part1_cost_at(align, positions):
     return sum([abs(p - align) for p in positions])
+
+
+def part2_cost_at(align, positions):
+    def cost(p):
+        d = abs(p-align)
+        return int(d*(d+1)/2)
+    return sum([cost(p) for p in positions])
 
 
 def main():
     positions = [int(s) for s in open("input.txt").readline().split(",")]
 
+    # Starting guess at arithmetic mean
     guess = int(sum(positions) / len(positions))
-    best_cost = cost_at(guess, positions)
+    best_cost = part1_cost_at(guess, positions)
 
+    # Search up and down until cost stops improving
     curr = guess + 1
-    cost = cost_at(curr, positions)
+    cost = part1_cost_at(curr, positions)
     while cost < best_cost:
         curr += 1
-        best_cost, cost = cost, cost_at(curr, positions)
+        best_cost, cost = cost, part1_cost_at(curr, positions)
 
     curr = guess - 1
-    cost = cost_at(curr, positions)
+    cost = part1_cost_at(curr, positions)
     while cost < best_cost:
         curr -= 1
-        best_cost, cost = cost, cost_at(curr, positions)
+        best_cost, cost = cost, part1_cost_at(curr, positions)
+
+    print(best_cost)
+
+    # part 2 search
+    best_cost = part2_cost_at(guess, positions)
+    curr = guess + 1
+    cost = part2_cost_at(curr, positions)
+    while cost < best_cost:
+        curr += 1
+        best_cost, cost = cost, part2_cost_at(curr, positions)
+
+    curr = guess - 1
+    cost = part2_cost_at(curr, positions)
+    while cost < best_cost:
+        curr -= 1
+        best_cost, cost = cost, part2_cost_at(curr, positions)
 
     print(best_cost)
 
