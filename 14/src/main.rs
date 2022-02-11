fd2fa2d13c92e8f467a95ed84e12673e158682d0
Daniel Jones <danielrjones1@gmail.com>
2022-02-11 20:41:31 +0100

    Puzzle 14 part 02

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -3,12 +3,12 @@ use std::{fmt, fs};
 use std::collections::{HashMap, HashSet};
 
 struct PairInsertionRules {
-    rules: HashMap<String, String>,
+    rules: HashMap<(char, char), char>,
 }
 
 impl PairInsertionRules {
-    fn result(&self, pair: &str) -> Option<&String> {
-        self.rules.get(pair)
+    fn result(&self, pair: (char, char)) -> char {
+        *self.rules.get(&pair).unwrap()
     }
 }
 
@@ -22,11 +22,12 @@ impl Polymer for String {
     fn apply(&mut self, rules: &PairInsertionRules) {
         let mut inserts = Vec::new();
         for i in 0..(self.len() - 1) {
-            inserts.push(rules.result(&self[i..(i + 2)]).unwrap());
+            let pair = (self.chars().nth(i).unwrap(), self.chars().nth(i + 1).unwrap());
+            inserts.push(rules.result(pair));
         }
 
         for i in 0..(inserts.len()) {
-            self.insert(2 * i + 1, inserts[i].chars().next().unwrap());
+            self.insert(2 * i + 1, inserts[i]);
         }
     }
 
@@ -54,20 +55,57 @@ fn main() {
     let mut lines = file_contents.lines();
     let mut polymer = lines.next().unwrap().to_string();
     let mut rules = PairInsertionRules { rules: HashMap::new() };
-
     lines.next(); // Ignore empty line
 
     for line in lines {
         let vec: Vec<&str> = line.splitn(2, " -> ").collect();
         let (pair, result) = (vec[0].to_string(), vec[1].to_string());
+        let pair = (pair.chars().nth(0).unwrap(), pair.chars().nth(1).unwrap());
+        let result = result.chars().nth(0).unwrap();
         rules.rules.insert(pair, result);
     }
 
-    println!("Template:     {}", polymer);
+    let mut polymer_part1 = polymer.clone();
+    // Part 1
+    println!("Template:     {}", polymer_part1);
     for step in 1..11 {
-        polymer.apply(&rules);
-        println!("After step {}: {}", step, polymer);
+        polymer_part1.apply(&rules);
+        println!("After step {}: {}", step, polymer_part1);
+    }
+    println!("{}", polymer_part1.score());
+
+    // Part 2
+    let mut pair_counts: HashMap<(char, char), usize> = HashMap::new();
+    for i in 0..(polymer.len() - 1) {
+        let (char1, char2) = (polymer.chars().nth(i).unwrap(), polymer.chars().nth(i + 1).unwrap());
+        pair_counts.entry((char1, char2)).and_modify(|v| *v += 1).or_insert(1);
     }
 
-    println!("{}", polymer.score());
+    for step in 1..41 {
+        let old_pair_counts = pair_counts.clone();
+        pair_counts.clear();
+        for ((char1, char2), count) in old_pair_counts.iter() {
+            let result = rules.result((*char1, *char2));
+            let pair1 = (*char1, result);
+            pair_counts.entry(pair1).and_modify(|c| *c += count).or_insert(*count);
+
+            let pair2 = (result, *char2);
+            pair_counts.entry(pair2).and_modify(|c| *c += count).or_insert(*count);
+        }
+    }
+    let mut char_counts: HashMap<char, usize> = HashMap::new();
+    for (pair, count) in pair_counts.iter() {
+        char_counts.entry(pair.0).and_modify(|c| *c += count).or_insert(*count);
+    }
+    // Add the last character in the chain on at the end
+    let last_polymer_char = polymer.chars().last().unwrap();
+    char_counts.entry(last_polymer_char).and_modify(|count| *count += 1);
+
+    let mut highest = 0;
+    let mut lowest = 1000000000000000;
+    for (_, count) in char_counts.iter() {
+        highest = max(highest, *count);
+        lowest = min(lowest, *count);
+    }
+    println!("{}", highest - lowest);
 }
