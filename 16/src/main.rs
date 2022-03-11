d1ec4e4b838962607313117f8db545f2e37167c6
Daniel Jones <danielrjones1@gmail.com>
2022-03-11 13:58:36 +0100

    Puzzle 16 part 02 - needs refactoring

--- 16/src/main.rs
+++ 16/src/main.rs
@@ -1,4 +1,4 @@
-use std::cmp::{max, min};
+use std::cmp::min;
 use std::fs;
 
 extern crate hex;
@@ -9,9 +9,9 @@ struct FileReader<'a> {
     read_bytes: usize,
 }
 
-fn read_bits(curr_value: u8, curr_bit: usize, num_bits: usize) -> u32 {
-    let mask = ((1u32 << num_bits) - 1) << (8 - curr_bit - num_bits);
-    ((curr_value as u32 & mask) >> (8 - curr_bit - num_bits))
+fn read_bits(curr_value: u8, curr_bit: usize, num_bits: usize) -> u64 {
+    let mask = ((1u64 << num_bits) - 1) << (8 - curr_bit - num_bits);
+    (curr_value as u64 & mask) >> (8 - curr_bit - num_bits)
 }
 
 impl<'a> FileReader<'a> {
@@ -33,10 +33,10 @@ impl<'a> FileReader<'a> {
         self.read_bytes * 8 + self.curr_bit
     }
 
-    fn read(&mut self, mut num_bits: usize) -> u32 {
+    fn read(&mut self, mut num_bits: usize) -> u64 {
         let remaining_bits = 8 - self.curr_bit;
         // Read the remaining bits from current char
-        let mut result = read_bits(self.data[0], self.curr_bit, min(num_bits, remaining_bits));
+        let result = read_bits(self.data[0], self.curr_bit, min(num_bits, remaining_bits));
 
         if num_bits >= remaining_bits {
             num_bits -= remaining_bits;
@@ -53,61 +53,77 @@ impl<'a> FileReader<'a> {
     }
 }
 
+fn read_n_packets(reader: &mut FileReader, version_sum: &mut u64, num_packets_to_read: usize) -> Vec<u64> {
+    let mut result = Vec::new();
+    for _ in 0..num_packets_to_read {
+        result.push(read_packet(reader, version_sum));
+    }
+    return result;
+}
 
-fn read_packets(reader: &mut FileReader, version_sum: &mut u32, stop_at_n_packets: Option<usize>, stop_after_n_bits: Option<usize>) {
+fn read_packets_from_n_bits(reader: &mut FileReader, version_sum: &mut u64, num_bits_to_read: usize) -> Vec<u64> {
     let start_bits_read = reader.bits_read();
-    let mut packets_read = 0;
+    let mut result = Vec::new();
+    while reader.bits_read() < num_bits_to_read + start_bits_read{
+        result.push(read_packet(reader, version_sum));
+    }
+    return result;
+}
 
-    while reader.can_read(6 + 5) {
-        match stop_after_n_bits {
-            Some(bit_count) => if reader.bits_read() >= bit_count + start_bits_read { break; }
-            _ => ()
-        }
-        match stop_at_n_packets {
-            Some(packet_count) => if packets_read >= packet_count { break; }
-            _ => ()
-        }
+fn read_packet(reader: &mut FileReader, version_sum: &mut u64) -> u64 {
+    if ! reader.can_read(6 + 5) { panic!("Not enough bits remaining"); }
 
-        let packet_version = reader.read(3);
-        let packet_type = reader.read(3);
-        *version_sum += packet_version;
-        packets_read += 1;
+    let packet_version = reader.read(3);
+    let packet_type = reader.read(3);
+    *version_sum += packet_version;
 
-        if packet_type == 4 { // Literal value
-            let mut value = 0;
-            loop {
-                let flag = reader.read(1);
-                value |= reader.read(4);
-                if flag == 1 {
-                    value <<= 4;
-                } else {
-                    break;
-                }
+    if packet_type == 4 { // Literal value
+        let mut value = 0;
+        loop {
+            let flag = reader.read(1);
+            value |= reader.read(4);
+            if flag == 1 {
+                value <<= 4;
+            } else {
+                break;
             }
-            println!("{}", value);
-        } else {
-            match reader.read(1) {
-                1 => {
-                    let packet_count = reader.read(11);
-                    read_packets(reader, version_sum, Some(packet_count as usize), None)
-                }
-                0 => {
-                    let bit_count = reader.read(15);
-                    read_packets(reader, version_sum, None, Some(bit_count as usize))
-                }
-                _ => panic!("unexpected length type ID")
-            };
+        }
+        value
+    } else {
+        let subpackets = match reader.read(1) {
+            1 => {
+                let packet_count = reader.read(11) as usize;
+                read_n_packets(reader, version_sum, packet_count)
+            }
+            0 => {
+                let bit_count = reader.read(15) as usize;
+                read_packets_from_n_bits(reader, version_sum, bit_count)
+            }
+            _ => panic!("unexpected length type ID")
+        };
+
+        return match packet_type {
+            0 => subpackets.iter().fold(0, |acc, x| acc + x),
+            1 => subpackets.iter().fold(1, |acc, x| acc * x),
+            2 => *subpackets.iter().min().unwrap(),
+            3 => *subpackets.iter().max().unwrap(),
+            5 => if subpackets[0] > subpackets[1] {1} else {0},
+            6 => if subpackets[0] < subpackets[1] {1} else {0},
+            7 => if subpackets[0] == subpackets[1] {1} else {0},
+            _ => panic!("Unknown operator type")
         }
     }
 }
 
 fn main() {
     let file_contents = fs::read_to_string("input.txt").unwrap();
+    // let file_contents = "9C0141080250320F1802104A08";
     let data = hex::decode(file_contents).unwrap();
     let mut reader = FileReader::new(data.as_slice());
 
     let mut version_sum = 0;
-    read_packets(&mut reader, &mut version_sum, None, None);
+    let result = read_packet(&mut reader, &mut version_sum);
 
     println!("Version sum: {}", version_sum);
+    println!("Result: {}", result);
 }
